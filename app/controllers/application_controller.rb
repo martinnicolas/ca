c63a456ad588aba4be86afa93a6eec90411ad998
martinnicolas <ttincho77@gmail.com>
2016-11-09 17:58:03 -0300

    Arreglo redireccionamiento al loguearse, after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
   	if current_user.role? :admin
-  		admin_path
+  		stored_location_for(resource) || admin_path
   	else
-  		index_path
+  		stored_location_for(resource) || root_path
   	end
   end
 end
