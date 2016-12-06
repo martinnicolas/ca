2300dc3454827bbe689648a31a93efd58549c40c
martinnicolas <ttincho77@gmail.com>
2016-12-06 18:47:19 -0300

    Soluciono problema de ordenamiento de valores en index

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,7 @@ class IndexController < ApplicationController
   def index
 
   	#Tomo todos los reclamos y los ordeno por mas valor
-  	@reclamos_destacados = Reclamo.select("reclamos.*, count(reclamos.id) as valoracion").joins('LEFT JOIN reclamo_usuarios ON reclamos.id = reclamo_usuarios.reclamo_id')
+  	@reclamos_destacados = Reclamo.select("reclamos.*, count(reclamos.id) as valoracion").joins('INNER JOIN reclamo_usuarios ON reclamos.id = reclamo_usuarios.reclamo_id')
   	.group('reclamos.id').order("valoracion DESC, fecha DESC")
 
   	#Tomo todos los reclamos y los ordeno por fecha
