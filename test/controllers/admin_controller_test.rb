c034be80b2313551164a41aec30d83db37203d5d
martinnicolas <ttincho77@gmail.com>
2017-12-14 20:13:27 -0300

    Modificaciones en tests

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -4,6 +4,8 @@ class AdminControllerTest < ActionController::TestCase
   include Devise::TestHelpers
   
   test "should get index" do
+  	sign_in @user
+	sign_in @user, scope: :admin
     get :index
     assert_response :success
   end
